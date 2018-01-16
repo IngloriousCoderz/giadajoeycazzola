0bff76c0d77e9e265b372b0c7112a76682c51ee7
IceOnFire <antony.mistretta@gmail.com>
2018-01-16 19:42:04 +0100

    Put common layout parts in main HTML

--- layouts/default.vue
+++ layouts/default.vue
@@ -50,20 +50,6 @@
   </nav>
 
   <nuxt/>
-
-  <footer class="footer">
-    <div class="container">
-      <div class="content has-text-centered">
-        <p>
-          &copy; Copyright 2017 Giada Joey Cazzola | Tutti i diritti riservati
-        </p>
-
-        <!-- // FB like button -->
-        <div class="fb-like" data-href="https://www.facebook.com/GiadaJoeyCazzolaPhotographer/" data-layout="standard" data-action="like" data-size="small" data-show-faces="true" data-share="true"></div>
-        <!-- // FB like button -->
-      </div>
-    </div>
-  </footer>
 </div>
 </template>
 
