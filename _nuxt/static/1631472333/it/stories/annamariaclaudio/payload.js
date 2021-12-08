c9f605e4eab9f1854a8315957aa9aa08b9befa76
IceOnFire <antony.mistretta@gmail.com>
2021-12-08 18:11:06 +0100

    Update stories and home

--- _nuxt/static/1631472333/it/stories/annamariaclaudio/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/it/stories/annamariaclaudio", {data:[{}],fetch:{},mutations:void 0});
---
+++ _nuxt/static/1638983356/it/stories/montainejonathan/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/it/stories/montainejonathan", {data:[{}],fetch:{},mutations:void 0});
