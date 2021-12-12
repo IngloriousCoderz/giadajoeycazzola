ab2f315f0fe9f6736eafd5725a11f760415fbe37
IceOnFire <antony.mistretta@gmail.com>
2021-12-12 11:43:15 +0100

    Upgrade analytics to Google Tag Manager

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,11 +1,11 @@
-import yaml from 'js-yaml'
 import fs from 'fs'
+import yaml from 'js-yaml'
 import { dateTimeFormats } from './locales/dateTimeFormats'
 
 const en = yaml.load(fs.readFileSync('./locales/en.yml'))
 const it = yaml.load(fs.readFileSync('./locales/it.yml'))
 
-module.exports = {
+export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
@@ -19,61 +19,61 @@ module.exports = {
       {
         name: 'viewport',
         content:
-          'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no'
+          'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no',
       },
       {
         hid: 'description',
-        name: 'description'
+        name: 'description',
         // content: 'Wedding photographer in Rome, Piedmont, Tuscany, the rest of Italy and Europe. Specialized in spontaneous photo and video reportage.'
         // content: 'Fotografa di matrimonio a Roma, in Piemonte, Toscana, resto di Italia ed Europa. Specializzata in reportage foto e video spontanei.'
       },
       { name: 'format-detection', content: 'telephone=no' },
       {
         hid: 'og:title',
-        property: 'og:title'
+        property: 'og:title',
         // content: 'Giada Joey Cazzola - Destination wedding photographer in Italy and Europe'
         // content: 'Giada Joey Cazzola - Fotografo reportage matrimionio Italia - Piemonte, Toscana, Roma'
       },
       {
         hid: 'og:description',
-        property: 'og:description'
+        property: 'og:description',
         // content: 'Wedding photographer in Rome, Piedmont, Tuscany, the rest of Italy and Europe. Specialized in spontaneous photo and video reportage.'
         // content: 'Fotografa di matrimonio a Roma, in Piemonte, Toscana, resto di Italia ed Europa. Specializzata in reportage foto e video spontanei.'
       },
       {
         hid: 'og:url',
         property: 'og:url',
-        content: 'https://www.giadajoeycazzola.com'
+        content: 'https://www.giadajoeycazzola.com',
       },
       {
         hid: 'og:image',
         property: 'og:image',
-        content: 'https://www.giadajoeycazzola.com/images/about_me.jpg'
+        content: 'https://www.giadajoeycazzola.com/images/about_me.jpg',
       },
       {
         hid: 'og:type',
         property: 'og:type',
-        content: 'website'
+        content: 'website',
       },
       {
         hid: 'fb:app_id',
         property: 'fb:app_id',
-        content: '1953139364946355'
-      }
+        content: '1953139364946355',
+      },
     ],
 
     link: [
       {
         hid: 'canonical',
         rel: 'canonical',
-        href: 'https://www.giadajoeycazzola.com'
+        href: 'https://www.giadajoeycazzola.com',
       },
       {
         rel: 'icon',
         type: 'image/x-icon',
-        href: '/favicon.ico'
-      }
-    ]
+        href: '/favicon.ico',
+      },
+    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
@@ -88,7 +88,7 @@ module.exports = {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/eslint
-    '@nuxtjs/eslint-module'
+    '@nuxtjs/eslint-module',
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
@@ -98,30 +98,33 @@ module.exports = {
     // https://go.nuxtjs.dev/pwa
     '@nuxtjs/pwa',
     '@nuxtjs/sitemap',
+    '@nuxtjs/google-gtag',
     [
       '@nuxtjs/i18n',
       {
         detectBrowserLanguage: {
           useCookie: true,
           cookieKey: 'i18n_redirected',
-          redirectOn: 'root' // recommended
-        }
-      }
-    ]
+          redirectOn: 'root', // recommended
+        },
+      },
+    ],
   ],
-
-  buildModules: ['@nuxtjs/google-analytics'],
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
   pwa: {
     manifest: {
-      lang: 'en'
-    }
+      lang: 'en',
+    },
   },
 
   sitemap: {
     hostname: 'https://www.giadajoeycazzola.com',
-    generate: true
+    generate: true,
+  },
+
+  'google-gtag': {
+    id: 'UA-61816704-2',
   },
 
   i18n: {
@@ -130,14 +133,10 @@ module.exports = {
     vueI18n: {
       fallbackLocale: 'en',
       messages: { en, it },
-      dateTimeFormats
-    }
-  },
-
-  googleAnalytics: {
-    id: 'UA-61816704-2'
+      dateTimeFormats,
+    },
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {}
+  build: {},
 }
